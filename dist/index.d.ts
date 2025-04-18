3cf0730d68dcbd178196b21f25f866c9626e3a5b
Garrett Keneau <gart@gart.sh>
2025-04-18 03:43:07 -0400

    add more exports

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,10 +1,51 @@
 import client from "./client";
 import { Logger as logger } from "./logger";
+import channel from "./channel";
+import callbackChannel from "./callbackChannel";
 export declare namespace Protocol {
     const Client: typeof client;
     const Logger: typeof logger;
+    const Channel: typeof channel;
+    const CallbackChannel: typeof callbackChannel;
+    const SubscriberChannel: callbackChannel<{
+        "channel:subscribe": {
+            request: {
+                channel: string;
+                key?: string;
+            };
+            response: {
+                success: boolean;
+                channel: string;
+                error?: string;
+            };
+        };
+        "channel:unsubscribe": {
+            request: {
+                channel: string;
+            };
+            response: {
+                success: boolean;
+                channel: string;
+                error?: string;
+            };
+        };
+    }>;
+    const PingChannel: callbackChannel<{
+        ping: {
+            request: {
+                message: string;
+            };
+            response: {
+                message: string;
+            };
+        };
+    }>;
 }
 export default Protocol;
 export { default as Client } from "./client";
 export { default as Logger } from "./logger";
+export { default as Channel } from "./channel";
+export { default as CallbackChannel } from "./callbackChannel";
+export { SubscriberChannel } from "./channels/subscriberChannel";
+export { PingChannel } from "./channels/pingChannel";
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 import client from "./client";
 import { Logger as logger } from "./logger";
+import channel from "./channel";
+import callbackChannel from "./callbackChannel";
+import { SubscriberChannel as subscriberChannel } from "./channels/subscriberChannel";
+import { PingChannel as pingChannel } from "./channels/pingChannel";
 
 export namespace Protocol {
     export const Client = client;
     export const Logger = logger;
+    export const Channel = channel;
+    export const CallbackChannel = callbackChannel;
+    export const SubscriberChannel = subscriberChannel;
+    export const PingChannel = pingChannel;
 }
 
 export default Protocol;
 export { default as Client } from "./client";
 export { default as Logger } from "./logger";
+export { default as Channel } from "./channel";
+export { default as CallbackChannel } from "./callbackChannel";
+export { SubscriberChannel } from "./channels/subscriberChannel";
+export { PingChannel } from "./channels/pingChannel";
